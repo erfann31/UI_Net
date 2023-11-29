0145e452b65bfb2cf484bc4bf86331135cd0074a
Erfan Nasri <erfannasri2@gmail.com>
2023-11-29 18:30:52 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,49 +1,54 @@
-import pydivert
+import scapy.all as scapy
+import time
 
+# Define custom protocol parameters
+CUSTOM_PROTOCOL_TYPE = 0x1234
+DESTINATION_MAC_INDEX = 1
+MESSAGE_INDEX = 2
 
-def send_message(mac_address, message):
-    with pydivert.WinDivert() as w:
-        # Construct the packet
-        packet = (
-                b"\x00\x01\x02\x03\x04\x05" +  # Source MAC address (dummy value)
-                bytes.fromhex(mac_address) +  # Destination MAC address
-                b"\x00\x00\x00\x00\x00\x00\x08\x00" +  # EtherType (0x0800 for IPv4)
-                b"\x45\x00\x00\x1c\x00\x00\x40\x00\x40\x11" +  # IPv4 header
-                b"\x00\x00\x00\x00" +  # Source IP address (dummy value)
-                b"\x00\x00\x00\x00" +  # Destination IP address (dummy value)
-                b"\x00\x00\x00\x00" +  # UDP header
-                message.encode()  # Message data
-        )
+# Function to send a message
+def send_message(message, destination_mac):
+    # Construct custom protocol packet
+    packet = scapy.Packet()
+    packet.type = CUSTOM_PROTOCOL_TYPE
+    packet.fields = [destination_mac, message]
 
-        # Send the packet
-        w.send(packet)
+    # Send the packet
+    scapy.sendp(packet, iface='eth0')
 
+# Function to receive messages
+def receive_message():
+    # Listen for packets with custom protocol type
+    packet = scapy.sniff(filter='type ' + str(CUSTOM_PROTOCOL_TYPE), iface='eth0', count=1)[0]
 
-def receive_messages():
-    with pydivert.WinDivert() as w:
-        for packet in w:
-            # Extract the message from the packet
-            message = packet[-len(packet) + 42:].decode()
+    # Extract message and sender's MAC address from the packet
+    message = packet.fields[MESSAGE_INDEX]
+    sender_mac = packet.fields[DESTINATION_MAC_INDEX]
 
-            # Display the message and source MAC address
-            print(f"Received Message: {message} (From: {':'.join(f'{byte:02x}' for byte in packet.source)})")
+    # Print the received message and sender's MAC address
+    print('Received message:', message)
+    print('Sender MAC address:', sender_mac)
 
+# Main program loop
+while True:
+    # Check if there is a message to send
+    send_mode = input('Enter \'send\' to send a message or \'receive\' to wait for a message: ')
 
-def main():
-    while True:
-        choice = input("Select an option (1 = Send, 2 = Receive): ")
+    if send_mode == 'send':
+        # Get message text and destination MAC address from user
+        message = input('Enter message to send: ')
+        destination_mac = input('Enter destination MAC address: ')
 
-        if choice == '1':
-            mac_address = input("Enter the destination MAC address: ")
-            message = input("Enter the message to send: ")
-            send_message(mac_address, message)
-            print("Message sent!")
-        elif choice == '2':
-            print("Waiting for messages...")
-            receive_messages()
-        else:
-            print("Invalid option. Try again.")
+        # Send the message
+        send_message(message, destination_mac)
 
+        # Wait for message confirmation
+        print('Waiting for message confirmation...')
+        while True:
+            confirmation = input('Did the message arrive successfully (yes/no)? ')
+            if confirmation == 'yes':
+                break
 
-if __name__ == "__main__":
-    main()
+    elif send_mode == 'receive':
+        # Receive and display messages
+        receive_message()
