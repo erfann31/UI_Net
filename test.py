fad42bd009d42b3cdf92a62ed1b35117f1bed970
Erfan Nasri <erfannasri2@gmail.com>
2023-11-29 20:05:39 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,54 +1,50 @@
-import scapy.all as scapy
-import time
+from scapy.all import *
+from scapy.layers.l2 import Ether
 
-# Define custom protocol parameters
-CUSTOM_PROTOCOL_TYPE = 0x1234
-DESTINATION_MAC_INDEX = 1
-MESSAGE_INDEX = 2
+
+# Define a custom protocol with necessary fields
+class CustomProtocol(Packet):
+    name = "CustomProtocol"
+    fields_desc = [
+        StrField("text", ""),
+        MACField("destination_mac", ETHER_ANY),
+    ]
+
 
 # Function to send a message
-def send_message(message, destination_mac):
-    # Construct custom protocol packet
-    packet = scapy.Packet()
-    packet.type = CUSTOM_PROTOCOL_TYPE
-    packet.fields = [destination_mac, message]
+def send_message(destination_mac, message):
+    packet = Ether(dst=destination_mac) / CustomProtocol(text=message, destination_mac=destination_mac)
+    sendp(packet, iface="Wi-Fi")
 
-    # Send the packet
-    scapy.sendp(packet, iface='eth0')
 
-# Function to receive messages
-def receive_message():
-    # Listen for packets with custom protocol type
-    packet = scapy.sniff(filter='type ' + str(CUSTOM_PROTOCOL_TYPE), iface='eth0', count=1)[0]
+# Function to receive and process messages
+def receive_message(packet):
+    if packet.haslayer(CustomProtocol):
+        custom_packet = packet[CustomProtocol]
+        source_mac = packet[Ether].src
+        text = custom_packet.text
+        print(f"Received message from {source_mac}: {text}")
 
-    # Extract message and sender's MAC address from the packet
-    message = packet.fields[MESSAGE_INDEX]
-    sender_mac = packet.fields[DESTINATION_MAC_INDEX]
 
-    # Print the received message and sender's MAC address
-    print('Received message:', message)
-    print('Sender MAC address:', sender_mac)
+# Sniff for incoming messages
+def listen_for_messages():
+    sniff(prn=receive_message, store=0)
+
 
 # Main program loop
-while True:
-    # Check if there is a message to send
-    send_mode = input('Enter \'send\' to send a message or \'receive\' to wait for a message: ')
+def main():
+    while True:
+        choice = input("Choose 'S' to send a message or 'R' to receive messages: ")
 
-    if send_mode == 'send':
-        # Get message text and destination MAC address from user
-        message = input('Enter message to send: ')
-        destination_mac = input('Enter destination MAC address: ')
+        if choice.upper() == 'S':
+            destination_mac = input("Enter the destination MAC address: ")
+            message = input("Enter the message to send: ")
+            send_message(destination_mac, message)
+        elif choice.upper() == 'R':
+            print("Listening for messages...")
+            listen_for_messages()
 
-        # Send the message
-        send_message(message, destination_mac)
 
-        # Wait for message confirmation
-        print('Waiting for message confirmation...')
-        while True:
-            confirmation = input('Did the message arrive successfully (yes/no)? ')
-            if confirmation == 'yes':
-                break
-
-    elif send_mode == 'receive':
-        # Receive and display messages
-        receive_message()
+if __name__ == '__main__':
+    main()
+    
