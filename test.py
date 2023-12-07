a20a93849477eca9b029faae83d1c8ca06f284f0
erfann31 <erfannasri2@gmail.com>
2023-12-07 17:48:24 +0330

    fix

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from scapy.all import *
 from scapy.layers.l2 import Ether
 
-IFACE='VMware Network Adapter VMnet8'
+PREM = "message:"
+PREA = "ack:"
+IFACE = 'VMware Network Adapter VMnet8'
+ack_received = False
+
+
 # Define a custom protocol with necessary fields
 class CustomProtocol(Packet):
     name = "CustomProtocol"
@@ -9,6 +14,7 @@ class CustomProtocol(Packet):
         StrField("text", ""),
         MACField("destination_mac", ETHER_ANY),
     ]
+
 
 class PacketModel:
     def __init__(self, src, dst, packet_type, raw_load):
@@ -18,9 +24,26 @@ class PacketModel:
         self.raw_load = raw_load
 
 
+def handle_acknowledgment(packet):
+    global ack_received
+    if isinstance(packet.payload, Raw) and packet.payload.load.startswith(b"ack:"):
+        ack_received = True
+        print("Acknowledgment Received")
+
+
+def wait_for_acknowledgment():
+    global ack_received
+    timeout = 10  # Timeout in seconds
+    start_time = time.time()
+
+    print("Waiting for acknowledgment...")
+    while not ack_received and (time.time() - start_time) < timeout:
+        sniff(iface=IFACE, prn=handle_acknowledgment, timeout=1)
+
+
 # Function to send a message
-def send_message(destination_mac, message):
-    packet = Ether(dst=destination_mac) / CustomProtocol(text=message, destination_mac=destination_mac)
+def send_message(destination_mac, pre, message):
+    packet = Ether(dst=destination_mac) / CustomProtocol(text=pre + message, destination_mac=destination_mac)
     packet.show()
     sendp(packet, iface=IFACE)
 
@@ -32,37 +55,47 @@ def receive_message(packet):
         dst_mac = packet.dst
         packet_type = packet.type
         raw_load = packet.load
-        stored_packet = PacketModel(src_mac, dst_mac, packet_type, raw_load)
+        # stored_packet = PacketModel(src_mac, dst_mac, packet_type, raw_load)
+        # # Accessing stored packet information
+        # print(f"Source MAC: {stored_packet.source}")
+        # print(f"Destination MAC: {stored_packet.destination}")
+        # print(f"Packet Type: {stored_packet.packet_type}")
+        # print(f"Raw Load: {stored_packet.raw_load}")
+        delimiter_index = raw_load.find(b':')
 
-        # Accessing stored packet information
-        print(f"Source MAC: {stored_packet.source}")
-        print(f"Destination MAC: {stored_packet.destination}")
-        print(f"Packet Type: {stored_packet.packet_type}")
-        print(f"Raw Load: {stored_packet.raw_load}")
+        # Extract the substring after the delimiter (if found)
+        if delimiter_index != -1:
+            extracted_message = raw_load[delimiter_index + 1:].split(b'\x00')[0].decode('utf-8', errors='ignore')
+            print(f"Extracted message: {extracted_message}")
+            send_message(src_mac, PREA, extracted_message)
 
 
 # Sniff for incoming messages
 def listen_for_messages():
     while True:
-        frames =sniff(iface=IFACE, prn=receive_message, count=1)
+        sniff(iface=IFACE, prn=receive_message, count=1)
         # print(frames[0])
-
 
 
 # Main program loop
 def main():
     while True:
-        choice = input("Choose 'S' to send a message or 'R' to receive messages: ")
+        choice = input("Choose 'S' to send a message or 'R' to receive messages (or 'Q' to quit): ")
 
         if choice.upper() == 'S':
-            message = "message:"
             destination_mac = input("Enter the destination MAC address: ")
-            message2 = input("Enter the message to send: ")
-            send_message(destination_mac, message+message2)
+            message = input("Enter the message to send: ")
+            send_message(destination_mac, PREM, message)
+            wait_for_acknowledgment()  # Wait for acknowledgment after sending message
+            if not ack_received:
+                print("No acknowledgment received.")
+            ack_received = False
         elif choice.upper() == 'R':
             print("Listening for messages...")
             listen_for_messages()
-
+        elif choice.upper() == 'Q':
+            print("Exiting the program...")
+            break
 
 if __name__ == '__main__':
     main()
